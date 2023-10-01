4f6add83b1e38d231ec22db8b16be0c869656a30
kristan <kristanbalin@gmail.com>
2023-10-01 22:47:26 +0800

    add a temporary search filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,17 +1,38 @@
-import React from 'react'
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-function Filter() {
+function Filter(props) {
+  const searchItems = async (e) => {
+    var filterValue = e.target.value;
 
-    return (
-        <Row className="justify-content-center">
-            <Col xs="10" lg="10" className="bg-warning">
-                Filter
-            </Col>
-        </Row>
+    await fetch(`http://localhost:3001/jobs?role_like=${filterValue}`, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Accept: "application/json",
+      },
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (jobJson) {
+        console.log(jobJson);
+        props.setJobs(jobJson);
+      });
+  };
 
-    )
+  return (
+    <Row className="justify-content-center">
+      <Col xs="10" lg="10" className="bg-warning">
+        <input
+          type="search"
+          className="form-control"
+          placeholder="Search..."
+          onChange={searchItems}
+        />
+      </Col>
+    </Row>
+  );
 }
 
-export default Filter
+export default Filter;
--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,63 +1,61 @@
-import React, { useState } from "react";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Stack from 'react-bootstrap/Stack';
+import React, { useState, useEffect } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Stack from "react-bootstrap/Stack";
 import Filter from "./Filter";
 
-
-
-
-
 const Lists = () => {
-
   const [jobs, setJobs] = useState([]);
 
-  const getData = () => {
-
-    fetch('http://localhost:3030/jobs?_limit=5'
-      , {
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        }
-      }
-    )
+  const getData = async () => {
+    await fetch("http://localhost:3001/jobs?_limit=5", {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Accept: "application/json",
+      },
+    })
       .then(function (response) {
         return response.json();
       })
       .then(function (jobJson) {
         // console.log(jobJson);
-        setJobs(jobJson)
+        setJobs(jobJson);
       });
-  }
+  };
 
-  getData()
+  useEffect(() => {
+    const interval = setInterval(() => {
+      getData();
+    }, 1000);
+    return () => clearInterval(interval);
+  });
 
   return (
     <div>
-     
       <Container>
         <Stack gap={4}>
-
-        <Filter />
+          <Filter setJobs={setJobs} />
 
           {jobs.map((job, index) => (
-
-            <Row className="justify-content-center" key={index} 
-                  data-role={job.role} data-level={job.level} 
-                      data-languages={job.languages} data-tools={job.tools}>
+            <Row
+              className="justify-content-center"
+              key={index}
+              data-role={job.role}
+              data-level={job.level}
+              data-languages={job.languages}
+              data-tools={job.tools}
+            >
               <Col xs="10" lg="10" className="bg-warning">
                 <div className="card-container">
                   <Row className="align-items-center">
-
                     <Col className="mt-3" lg="6">
                       <div>
                         <span className="me-4">{job.company}</span>
                         <span className="me-2">{job.new}</span>
                         <span className="me-2">{job.featured}</span>
                       </div>
-                      <h6 className="my-1" >{job.position}</h6>
+                      <h6 className="my-1">{job.position}</h6>
                       <div>
                         <span className="me-2">{job.postedAt}</span>
                         <span className="me-2">&#8226;</span>
@@ -71,26 +69,29 @@ const Lists = () => {
                       <span className="d-inline-block me-3">{job.role}</span>
                       <span className="d-inline-block me-3">{job.level}</span>
                       {job.languages.map((language, index) => {
-                        return <span className="d-inline-block me-3" key={index}>{language}</span>
+                        return (
+                          <span className="d-inline-block me-3" key={index}>
+                            {language}
+                          </span>
+                        );
                       })}
 
-                       {job.tools.map((tool, index) => {
-                        return <span className="d-inline-block me-3" key={index}>{tool}</span>
+                      {job.tools.map((tool, index) => {
+                        return (
+                          <span className="d-inline-block me-3" key={index}>
+                            {tool}
+                          </span>
+                        );
                       })}
                     </Col>
-
                   </Row>
-
                 </div>
               </Col>
             </Row>
           ))}
-
         </Stack>
       </Container>
-
-
-    </div >
+    </div>
   );
 };
 
